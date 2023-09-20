971e1a7280440728ae50b3711e456941ac7d1227
shubham <shubham.singhgkp07@gmail.com>
2023-09-20 09:23:41 +0530

    v3

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -1,18 +1,12 @@
 package com.driver;
 
-public class DeluxePizza extends Pizza {
+public class DeluxePizza extends Pizza
+{
     public DeluxePizza(Boolean isVeg)
     {
         super(isVeg);
 
-        if(isVeg)
-        {
-            addExtraCheese();
-            addExtraToppings();
-        }
-        else {
-            addExtraToppings();
-            addExtraCheese();
-        }
+        addExtraCheese();
+        addExtraToppings();
     }
 }
